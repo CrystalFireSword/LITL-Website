07181b9378a1353bc4c9096f59972d94bb4bff88
baggie11 <nbaggie2027@gmail.com>
2024-11-22 20:02:12 +0530

    before pull

--- frontend-app/src/components/Order.jsx
+++ frontend-app/src/components/Order.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import '../Order.css';
 
 const Order = () => {
     return(
         <div className="order-form-class">
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScPvIDgcp8AsrvMQu1YXhO8J_h2n0kLHCyQm5c3pgNKmr_AJw/viewform?embedded=true" width="640" height="3526" frameborder="0" marginheight="0" marginwidth="0"/>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScPvIDgcp8AsrvMQu1YXhO8J_h2n0kLHCyQm5c3pgNKmr_AJw/viewform?embedded=true" width="640" height="3526" frameborder="0"  marginwidth="0" />
         </div>
         
     );
